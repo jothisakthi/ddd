45704d8bed3acfbbe1328e7d9c5a05e5d7822be4
jothisakthi <jothisakthivel23@gmail.com>
2016-08-19 03:35:47 -0700

    cv
    vb

---
+++ strrev.java
@@ -0,0 +1,13 @@
+package quiz;
+
+public class guvi {
+public static void main(String args[]){
+	String s="kavitha";
+	String result=" ";
+	for(int i=s.length()-1;i>=0;i--){
+	result=result+s.charAt(i);
+	
+	}
+	System.out.println(result);
+}
+}
